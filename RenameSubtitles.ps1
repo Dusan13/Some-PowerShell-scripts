73327a7c88d2e0b4466f133ff33f5e7ca337e6bd
Dusan <dusanjovan13@gmail.com>
2019-06-01 20:55:46 +0200

    now support other formats then s01e01

--- RenameSubtitles.ps1
+++ RenameSubtitles.ps1
@@ -3,6 +3,26 @@ param(
     [string]$Path
 )
 
+function get-NthTwoNumber {
+    param (
+        [string] $word,
+        [int] $N
+    )
+    $reg = '\d\d'
+    $count = 0
+    for ($i = 0; $i -lt $word.length-1; $i++) {
+        $subWord = $word.Substring($i, 2)
+        if ($subWord -match $reg) {
+            $count++
+            if ($count -eq $N) {
+                return $subWord
+            }
+        }
+    }
+}
+
+
+#PROGRAM
 $lastLocation = Get-Location
 
 if (!([string]::IsNullOrEmpty($Path))) {
@@ -15,7 +35,7 @@ if (!([string]::IsNullOrEmpty($Path))) {
     Set-Location $Path
 }
 
-$regex = 's\d\de\d\d'
+$regex = 's?\d\dx?X?.?-?_?e?\d\d'
 
 $subtitleExtensionArray = '.srt', '.sub', '.sbv'
 $videoExtensionArray = '.avi', '.mkv', '.mp4', '.flv', '.swf', '.mov', '.wmv'
@@ -23,7 +43,13 @@ $videoExtensionArray = '.avi', '.mkv', '.mp4', '.flv', '.swf', '.mov', '.wmv'
 $allSubtitleFiles = get-childitem $Path | Where-Object { $subtitleExtensionArray.Contains($_.extension) }
 $allVideoFiles = get-childitem $Path | Where-Object { $videoExtensionArray.Contains($_.extension) }
 
+foreach ($sub in $allSubtitleFiles) {
+    $s = get-NthTwoNumber -word $sub.name -N 1
+    $e = get-NthTwoNumber -word $sub.name -N 2
+    Rename-Item $sub -NewName ('s{0}e{1}.{2}' -f $s, $e, $sub.Extension)
+}
 
+$allSubtitleFiles = get-childitem $Path | Where-Object { $subtitleExtensionArray.Contains($_.extension) }
 
 foreach ($subFile in $allSubtitleFiles) {
     $subFile.name -match $regex > $null
@@ -40,3 +66,4 @@ foreach ($subFile in $allSubtitleFiles) {
 }
 
 Set-Location $lastLocation
+
